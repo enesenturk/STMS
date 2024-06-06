94c66b2b8414d5da42f0d701ae82b85641019261
NS <enesenturk61@gmail.com>
2024-06-06 12:09:51 +0300

    entity update

--- NS.STMS.Entity/Context/t_user.cs
+++ NS.STMS.Entity/Context/t_user.cs
@@ -5,35 +5,39 @@ namespace NS.STMS.Entity.Context;
 public partial class t_user : IEntity
 {
 
-    public string name { get; set; }
+	public string name { get; set; }
 
-    public string surname { get; set; }
+	public string surname { get; set; }
 
-    public string email { get; set; }
+	public string email { get; set; }
 
-    public DateOnly date_of_birth { get; set; }
+	public DateOnly date_of_birth { get; set; }
 
-    public int t_county_id { get; set; }
+	public int t_county_id { get; set; }
 
-    public string image_base64 { get; set; }
+	public string image_base64 { get; set; }
 
-    public string password { get; set; }
+	public string password { get; set; }
 
-    public byte[] password_salt { get; set; }
+	public byte[] password_salt { get; set; }
 
-    public int t_property_id_user_type { get; set; }
+	public int t_property_id_user_type { get; set; }
 
-    public bool accepted_terms { get; set; }
+	public bool accepted_terms { get; set; }
 
-    public DateTime? accepted_terms_at { get; set; }
+	public DateTime? accepted_terms_at { get; set; }
 
-    public bool verified_email { get; set; }
+	public bool verified_email { get; set; }
 
-    public DateTime? verified_email_at { get; set; }
+	public DateTime? verified_email_at { get; set; }
 
-    public virtual t_county t_county { get; set; }
+	public int login_fail_count { get; set; }
 
-    public virtual t_property t_property_id_user_typeNavigation { get; set; }
+	public bool needs_change_password { get; set; }
 
-    public virtual ICollection<t_student> t_students { get; set; } = new List<t_student>();
+	public virtual t_county t_county { get; set; }
+
+	public virtual t_property t_property_id_user_typeNavigation { get; set; }
+
+	public virtual ICollection<t_student> t_students { get; set; } = new List<t_student>();
 }
